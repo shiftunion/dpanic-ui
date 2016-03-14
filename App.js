dce6caacbd87f70ee7820f0efbb9e91d014e8889
ShiftUnion <nick.cahill@gmail.com>
2016-03-14 17:06:00 +1300

    Added some props the move into my app componant

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
     render(){
-        return <div>Killme!</div>
+        let txt = this.props.txt
+        return <h1>{this.props.txt}{this.props.cat}</h1>
+
     }
 }
+
+// Kinda a type definition
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+// just some sensible defaults for props
+App.defaultProps = {
+    txt: 'Default props val',
+    cat: 23
+}
+
+
+ReactDOM.render(
+    <App cat={12} txt="this is the props value" />,
+    document.getElementById('app')
+)
+
+// has no state!
+//const App = () => <h1>Hello Eggheads</h1>
 
 export default App
