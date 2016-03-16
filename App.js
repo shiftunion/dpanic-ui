3508640d537eb6f5bfb30da75941fa5fe48bd2a6
ShiftUnion <nick.cahill@gmail.com>
2016-03-16 20:26:21 +1300

    09 - Component Lifecycle - Mounting Basics

--- App.js
+++ App.js
@@ -2,23 +2,66 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {val: 0};
+        this.update = this.update.bind(this);
+    }
+
+    update() {
+        this.setState({val: this.state.val + 1})
+    }
+
+    componentWillMount(){
+        console.log('mounting')
+    }
+
+    componentDidMount() {
+        console.log('mounted')
+    }
+
+    componentWillUnmount() {
+        console.log('bye')
+    }
+
     render() {
-        return <Button1>I <Heart/> React</Button1>
+        console.log('rendering!');
+        return(
+            <button onClick={this.update}>{this.state.val}</button>
+        )
+
     }
 }
 
-class Button1 extends React.Component {
-    render() {
 
-        return <button>{this.props.children}</button>
+
+
+class Wrapper extends React.Component{
+    constructor() {
+        super();
+    }
+    mount(){
+        ReactDOM.render(React.createElement(App), document.getElementById('a'))
+        console.log('dude')
+    }
+    unmount(){
+        ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+    }
+    render(){
+        return (
+            <div>
+                <button onClick={this.mount.bind(this)}>Mount</button>
+                <button onClick={this.unmount.bind(this)}>Unmount</button>
+                <div id="a"></div>
+            </div>
+        )
     }
 }
 
-const Heart = () => <span className="glyphicon glyphicon-heart"></span>
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-)
+    <Wrapper />,
+    document.getElementById('ap'));
 
-export default App
+export default Wrapper
+App
